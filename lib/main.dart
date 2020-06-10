b3f330542412da016cc3bfdfeff9fc71c59aa519
rocketman <ryomajk@gmail.com>
2020-06-10 11:33:23 +0900

    xcode modified

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class _MyAppState extends State<MyApp> {
             return MaterialPageRoute(builder: (context) => StartUpView());
         }
       },
-      home: VoteSelectView(),
+      home: StartUpView(),
     );
   }
 }
