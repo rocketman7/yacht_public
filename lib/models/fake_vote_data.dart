518a74c3ea4879a436ed1abdebd94afabb592fea
rocketman <ryomajk@gmail.com>
2020-06-12 02:06:20 +0900

    listView remove & add tested

---
+++ lib/models/fake_vote_data.dart
@@ -0,0 +1,63 @@
+const VOTE_INFO = [
+  {
+    'voteDate': '20200901',
+    'voteCount': 5,
+    'voteResult': [1, 2, 1, 1, 2],
+  }
+];
+
+var startDate = DateTime.utc(2020, 9, 1, 7, 0, 0); // KST 2020/09/01 16:00
+var endDate = DateTime.utc(2020, 9, 1, 23, 50, 0); // KST 2020/09/02 08:50
+
+var VOTE_DATA = [
+  {
+    'title': '9월 2일 코스피 방향은?',
+    'voteImg':
+        'https://img5.yna.co.kr/photo/yna/YH/2020/01/29/PYH2020012920480032500_P4.jpg',
+    'voteStartDate': startDate,
+    'voteEndDate': endDate,
+    'voteChoices': ['상승', '하락'],
+    'choiceCounts': 2,
+    'Result': null
+  },
+  {
+    'title': '9월 2일 수익률 승자는?',
+    'voteImg':
+        'https://img5.yna.co.kr/photo/yna/YH/2020/01/29/PYH2020012920480032500_P4.jpg',
+    'voteStartDate': startDate,
+    'voteEndDate': endDate,
+    'voteChoices': ['네이버', '카카오'],
+    'choiceCounts': 2,
+    'Result': null
+  },
+  {
+    'title': '9월 2일 환율 방향은?',
+    'voteImg':
+        'https://img5.yna.co.kr/photo/yna/YH/2020/01/29/PYH2020012920480032500_P4.jpg',
+    'voteStartDate': startDate,
+    'voteEndDate': endDate,
+    'voteChoices': ['상승', '하락'],
+    'choiceCounts': 2,
+    'Result': null
+  },
+  {
+    'title': '9월 2일 수익률 승자는?',
+    'voteImg':
+        'https://img5.yna.co.kr/photo/yna/YH/2020/01/29/PYH2020012920480032500_P4.jpg',
+    'voteStartDate': startDate,
+    'voteEndDate': endDate,
+    'voteChoices': ['대웅제약', '일양약품'],
+    'choiceCounts': 2,
+    'Result': null
+  },
+  {
+    'title': '9월 2일 수익률 승자는?',
+    'voteImg':
+        'https://img5.yna.co.kr/photo/yna/YH/2020/01/29/PYH2020012920480032500_P4.jpg',
+    'voteStartDate': startDate,
+    'voteEndDate': endDate,
+    'voteChoices': ['신풍제약', '부광약품'],
+    'choiceCounts': 2,
+    'Result': null
+  },
+];
