a7baee914167aa9bddfdc9e03352bdb8716ded36
rocketman <ryomajk@gmail.com>
2022-01-05 07:38:20 +0900

    commit

---
+++ packages/native_admob_flutter-master/example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<native_admob_flutter/NativeAdmobFlutterPlugin.h>)
+#import <native_admob_flutter/NativeAdmobFlutterPlugin.h>
+#else
+@import native_admob_flutter;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [NativeAdmobFlutterPlugin registerWithRegistrar:[registry registrarForPlugin:@"NativeAdmobFlutterPlugin"]];
+}
+
+@end
